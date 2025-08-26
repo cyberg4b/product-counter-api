d5fffbb91e51d895463306347cdda1536cb79a62
Meski <souzamesquita26@gmail.com>
2025-08-26 16:32:08 -0300

    adding .env

--- src/main.py
+++ src/main.py
@@ -5,17 +5,18 @@ from ultralytics import YOLO
 import cv2
 import os
 import uvicorn
+from dotenv import load_dotenv
 
-# Configurações
-API_KEY = "6ffkqsR7ASZ9dMagBMFBAQp46Cf4ZE6t"  # troque por algo forte
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
 API_KEY_NAME = "access_token"
 
 app = FastAPI()
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=True)
 
-model = YOLO("yolo11m.pt")  # ou seu modelo customizado
+model = YOLO("yolo11m.pt")
 
-# Função para validar a chave
 def get_api_key(api_key: str = Security(api_key_header)):
     if api_key != API_KEY:
         raise HTTPException(status_code=403, detail="Acesso negado 🚫")
@@ -25,7 +26,7 @@ def get_api_key(api_key: str = Security(api_key_header)):
 @app.post("/contar-produtos")
 async def contar_produtos(
     file: UploadFile = File(...),
-    api_key: str = Depends(get_api_key)  # 🔒 segurança aplicada
+    api_key: str = Depends(get_api_key) 
 ):
     # Salvar arquivo temporário
     contents = await file.read()
@@ -34,7 +35,6 @@ async def contar_produtos(
     with open(arquivo_temp, "wb") as f:
         f.write(contents)
 
-    # Ler imagem
     img = cv2.imread(arquivo_temp)
 
     # Fazer inferência
@@ -44,15 +44,15 @@ async def contar_produtos(
     cv2.putText(anotada, f"Produtos: {contador}", (20, 50),
                 cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 3)
 
-    # Remover arquivo temporário
     os.remove(arquivo_temp)
 
     return JSONResponse({"quantity": contador})
 
 
 @app.get("/helloworld")
-def obter_imagem(api_key: str = Depends(get_api_key)):  # 🔒 protegido também
+def obter_imagem(api_key: str = Depends(get_api_key)):
     return {"msg": "API FUNCIONANDO ✅"}
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=10000)
